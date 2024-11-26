dbed76e5f0aa7130ca40de5bb62458ece65bf0ee
daraazinovic <darko.azinovic@gmail.com>
2024-11-26 17:44:48 +0100

    Izrada tablica

--- Zavsrni sad-Trgovina rukotvorina.sql
+++ Zavsrni sad-Trgovina rukotvorina.sql
@@ -7,10 +7,11 @@ create database trgovinarukotvorina collate Croatian_CI_AS;
 go
 use trgovinarukotvorina;
 go
+
 create table proizvodi(
 sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
-izradujeseod varchar(50) references materijali(sifra),
+izradujeseod varchar(50),
 cijena int not null,
 namjena varchar(50) not null 
 );
@@ -18,16 +19,18 @@ namjena varchar(50) not null
 create table materijali(
 sifra int not null primary key identity(1,1),
 naziv varchar(30)not null,
-vrsta varchar(30) references vrste(sifra)
+vrsta varchar(50)not null
+);
+
+create table sastavnice(
+sifra int not null primary key identity(1,1),
+proizvodi int not null references proizvodi(sifra),
+materijali int not null references materijali(sifra)
 );
 
 create table vrste(
 sifra int not null primary key identity(1,1),
-naziv varchar(50) references materijali(sifra)
+naziv int not null references materijali(sifra),
+sastav varchar(50)not null
 );
 
-create table sastavnica(
-sifra int not null primary key identity(1,1),
-proizvodi varchar(50) references proizvodi(sifra),
-materijali varchar(50) references materijali(sifra)
-);
