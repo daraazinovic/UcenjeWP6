232e90597ebd3cdf909d5e0a44e4251c8de8092c
daraazinovic <darko.azinovic@gmail.com>
2024-11-26 18:45:17 +0100

    Uptade završnog rada

--- Zavsrni sad-Trgovina rukotvorina.sql
+++ Zavsrni sad-Trgovina rukotvorina.sql
@@ -1,4 +1,4 @@
---use master
+use master
 
 go
 drop database if exists trgovinarukotvorina;
@@ -30,7 +30,29 @@ materijali int not null references materijali(sifra)
 
 create table vrste(
 sifra int not null primary key identity(1,1),
-naziv int not null references materijali(sifra),
+materijal int not null references materijali(sifra),
 sastav varchar(50)not null
 );
+
+
+insert into proizvodi(naziv,izradujeseod,cijena,namjena)
+values('Zahvalnice za svatove', 'vosak', 15, 'Zahvalnica za uzvanike svatova');
+
+insert into proizvodi(naziv,izradujeseod,cijena,namjena)
+values('Kuæica sa crkvom','beton',15,'Ukras za domove');
+
+insert into proizvodi(naziv,izradujeseod,cijena,namjena)
+values('Kapela sa mirisnom svijeæom','beton i svijeæa',10,'Ukras za razne prigode');
+
+insert into proizvodi(naziv,izradujeseod,cijena,namjena)
+values('Mini vaza','beton i umjetno cvijeæe',10,'Ukras za prigode i domove');
+
+insert into proizvodi(naziv,izradujeseod,cijena,namjena)
+values('Posudice za luèice', 'beton i luèica', 15, 'Ukras za dom')
+
+insert into proizvodi(naziv,izradujeseod,cijena,namjena)
+values('Figura bicikli', 'beton', 15, 'Ukras za dom')
+
+insert into proizvodi(naziv,izradujeseod,cijena,namjena)
+values('Set kuæice i vaze sa luèicom', 'beton,vosak i cvijeæe', 20,'Ukras za dom')
 
