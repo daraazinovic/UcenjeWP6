a0f15659c5109b861b185ff3fc0fc92c37921f80
daraazinovic <darko.azinovic@gmail.com>
2024-11-29 22:50:50 +0100

    Create Vježba 2 sql.sql

---
+++ Vježba 2 sql.sql
@@ -0,0 +1,20 @@
+﻿use master;
+go
+
+drop database if exists webshop;
+go
+
+create database webshop;
+go
+
+use webshop;
+go
+
+create table proizvodi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+datumnabave datetime,
+cijena decimal(18,2),
+aktivan varchar (30)
+);
+
