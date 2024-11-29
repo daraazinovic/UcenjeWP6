6d93d98d731c419d5f202b9cfe31439b112e99e9
daraazinovic <darko.azinovic@gmail.com>
2024-11-29 22:51:27 +0100

    Update Vježba 2 sql.sql

--- Vježba 2 sql.sql
+++ Vježba 2 sql.sql
@@ -18,3 +18,26 @@ cijena decimal(18,2),
 aktivan varchar (30)
 );
 
+create table kupci(
+sifra int not null primary key identity(1,1),
+ime varchar(30) not null,
+prezime varchar(30) not null,
+ulica varchar(30) not null,
+mjesto varchar(30) not null
+);
+
+
+create table racuni(
+sifra int not null primary key identity(1,1),
+datum datetime,
+kupac int not null references kupci(sifra),
+status varchar(30) not null
+);
+
+create table stavke(
+racun int not null references racuni(sifra),
+proizvod int not null references proizvodi(sifra),
+kolicina int not null,
+cijena decimal(18,2)
+);
+
