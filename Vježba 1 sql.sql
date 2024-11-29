c50ac16487f8011573cc20a3b168f07d3b703e7f
daraazinovic <darko.azinovic@gmail.com>
2024-11-29 22:15:59 +0100

    Create Vježba 1 sql.sql

---
+++ Vježba 1 sql.sql
@@ -0,0 +1,28 @@
+﻿use master;
+go
+
+drop database if exists tvrtka;
+go
+
+create database tvrtka;
+go
+
+use tvrtka;
+
+create table zaposlenici(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+datumrodenja datetime,
+placa decimal(18,2),
+invalid varchar (50)
+);
+
+
+create table slika(
+sifra int not null primary key identity(1,1),
+zaposlenik int not null references zaposlenici(sifra),
+rednibroj int not null references zaposlenici(sifra),
+putanja varchar(50)
+);
+
