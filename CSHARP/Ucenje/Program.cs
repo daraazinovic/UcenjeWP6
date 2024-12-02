a307e374030ac651093f08d923fe311fca2f2ed0
Darko Azinović <darko.azinovic@gmail.com>
2024-12-02 17:51:19 +0100

    Upoznavanje s sintaksom

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//komentar jedna linija
+
+/*
+
+komentar više linija
+
+*/
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Pozdrav Svijetu ćčđćđ ćđćžš");
+Console.WriteLine("Hello Osijek");
+
