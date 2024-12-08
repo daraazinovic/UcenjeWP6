ae1e586d864e4916506411adc77b92f80e12b22e
daraazinovic <darko.azinovic@gmail.com>
2024-12-08 11:32:41 +0100

    Rješenje

--- CSHARP/Ucenje/E04Z3.cs
+++ CSHARP/Ucenje/E04Z3.cs
@@ -13,21 +13,28 @@ namespace Ucenje
             Console.Write("Unesi prvi cijeli broj: ");
             int broj1 = int.Parse(Console.ReadLine());
 
-                Console.Write("Unesi drugi cijeli broj");
+            Console.Write("Unesi drugi cijeli broj: ");
             int broj2 = int.Parse(Console.ReadLine());
 
-                bool uvjet = broj1 < broj2;
-            Console.WriteLine(uvjet);
 
-            if (uvjet)
-            {
-                Console.WriteLine("Manji broj je: ");
-            }
+            //int manjibroj = Math.Min(broj1, broj2);
+            //Console.WriteLine("Manji broj je: {0}", manjibroj);
+
+            Console.Write("Unesi treći cijeli broj: ");
+            int broj3 = int.Parse(Console.ReadLine());
+
+            int najmanjibroj = Math.Min(broj1, Math.Min( broj2, broj3));
+            Console.WriteLine("Najmanji broj je {0}", najmanjibroj );
+
+
+           
+
+
 
 
         }
 
-        
+
 
 
 
