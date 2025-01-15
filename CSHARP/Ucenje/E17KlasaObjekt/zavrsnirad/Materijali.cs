fb870425488e6adf16a8d1e47bfbe9385cf50fc4
daraazinovic <darko.azinovic@gmail.com>
2025-01-15 14:08:38 +0100

    Zavrsni rad

---
+++ CSHARP/Ucenje/E17KlasaObjekt/zavrsnirad/Materijali.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje.E17KlasaObjekt.zavrsnirad
+{
+    public class Materijali
+    {
+        public int Sifra { get; set; }
+
+        public string Naziv { get; set; }
+
+        public string Vrsta { get; set; }
+    }
+}
---
+++ CSHARP/Ucenje/E17KlasaObjekt/zavrsnirad/Sastavnice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje.E17KlasaObjekt.zavrsnirad
+{
+    public class Sastavnice
+    {
+        public int Sifra { get; set; }
+
+        public string Proizvod { get; set; }
+
+        public string Materijal { get; set; }
+    }
+}
