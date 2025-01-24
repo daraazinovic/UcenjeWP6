2f902f994656092ecb5e44a3554795307a0c86f7
daraazinovic <darko.azinovic@gmail.com>
2025-01-25 00:46:41 +0100

    Ljubavni kalkulator

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -2,7 +2,9 @@
 
 using Ucenje;
 
-new Ucenje.E20KonzolnaAplikacija.Izbornik();
+LjubavniKalkulator.Izvedi();
+
+//new Ucenje.E20KonzolnaAplikacija.Izbornik();
 
 //new Ucenje.E19GenericiLambdaEkstenzije.Program();
 
