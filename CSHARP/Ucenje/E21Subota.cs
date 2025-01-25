d036801296e5566f2dc6f017f53b967892fd3d83
daraazinovic <darko.azinovic@gmail.com>
2025-01-25 10:56:47 +0100

    Pomoć pri ljubavi

--- CSHARP/Ucenje/E21Subota.cs
+++ CSHARP/Ucenje/E21Subota.cs
@@ -9,12 +9,43 @@ namespace Ucenje
 {
     class E21Subota
     {
+        private int ponovilose;
 
         public E21Subota()
         {
             //Console.WriteLine("Hello from E21Subota");
 
-            SlucajniBrojevi();
+            //SlucajniBrojevi();
+            //SlucanjiDatumi();
+            Ljubav();
+        }
+
+        private void Ljubav()
+        {
+            var ona = "Marta";
+            var on = "Manuel";
+
+            var izraz = ona.Trim().ToLower() + on.Trim().ToLower();
+
+            Console.WriteLine(izraz);
+
+            int[] brojevi = new int[izraz.Length];
+
+            for (int i = 0; i < izraz.Length; i++)
+            {
+                ponovilose = 0;
+                for (int j =; j < izraz.Length; j++)
+                {
+                    if (izraz[i] == izraz[j])
+                    {
+                        ponovilose++;
+                    }
+                }
+                brojevi[i] = ponovilose;
+            }
+
+            Console.WriteLine(string.Join('|', izraz.ToArray()));
+            Console.WriteLine(string.Join('|', brojevi));
         }
 
         private void SlucajniBrojevi()
@@ -24,6 +55,27 @@ namespace Ucenje
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(rnd.Next(20, 31));
+            }
+        }
+
+
+        private void SlucanjiDatumi()
+        {
+            var random = new Random();
+            
+            for (int i = 0; i < 100; i++)
+
+            {
+                try
+                {
+                    var d = new DateTime(2023, random.Next(1, 13), random.Next(1, 32));
+                    Console.WriteLine((i+1)+ ": "+ d.ToString("yyyy-MM-dd"));
+                }
+                catch
+                {
+                    i--;
+                }
+                
             }
         }
     }
