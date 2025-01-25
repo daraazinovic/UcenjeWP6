1061a2fcbf86e49931bdf0534e300e79b0016432
daraazinovic <darko.azinovic@gmail.com>
2025-01-25 08:46:34 +0100

    Ljubavni kalkulator

--- Vježbe/LjubavniKalkulator.cs
+++ Vježbe/LjubavniKalkulator.cs
@@ -11,13 +11,12 @@ namespace Ucenje
         public static void Izvedi()
         {
 
-            
-        
+
             string ime1 = UcitajIme("Unesite prvo ime: ");
             string ime2 = UcitajIme("Unesite drugo ime: ");
 
-            int postotak1 = IzracunajPostotakImena(ime1);
-            int postotak2 = IzracunajPostotakImena(ime2);
+            int postotak1 = IzracunajPostotakImena(ime1, 0, 0); // Poziv rekurzivne metode
+            int postotak2 = IzracunajPostotakImena(ime2, 0, 0); // Poziv rekurzivne metode
 
             int ukupniPostotak = (postotak1 + postotak2) / 2;
 
@@ -25,7 +24,7 @@ namespace Ucenje
             Console.WriteLine($"{ime2} ima {postotak2}% 'ljubavi'.");
             Console.WriteLine($"\n{ime1} i {ime2} imaju {ukupniPostotak}% kompatibilnosti (čisto iz zabave!).");
 
-            //Dodatni "komentar" ovisno o postotku
+            //Dodatni "komentar" ovisno o postotku 
             if (ukupniPostotak < 30)
             {
                 Console.WriteLine("Malo je tu ljubavi u zraku...");
@@ -44,21 +43,21 @@ namespace Ucenje
             }
         }
 
-        private static int IzracunajPostotakImena(string ime)
+        private static int IzracunajPostotakImena(string ime, int indeks, int zbroj)
         {
-            int zbroj = 0;
-            // Petlja za zbrajanje susjednih slova
-            for (int i = 0; i < ime.Length - 1; i++)
+            // Baza rekurzije: ako smo došli do kraja imena (ili pretposljednjeg znaka)
+            if (indeks >= ime.Length - 1)
             {
-                zbroj += (int)ime[i] + (int)ime[i + 1];
-            }
-            //Ako je broj slova neparan dodajemo zadnje slovo
-            if (ime.Length % 2 != 0)
-            {
-                zbroj += (int)ime[ime.Length - 1];
+                if (ime.Length % 2 != 0 && indeks < ime.Length)
+                {
+                    zbroj += (int)ime[indeks];
+                }
+                return zbroj % 101;
             }
 
-            return zbroj % 101;
+            // Rekurzivni korak: zbroji trenutni i sljedeći znak i pozovi metodu za sljedeći indeks
+            zbroj += (int)ime[indeks] + (int)ime[indeks + 1];
+            return IzracunajPostotakImena(ime, indeks + 2, zbroj); // Pozivamo funkciju sa indeksom + 2 jer zbrajamo dva slova odjednom
         }
 
         private static string UcitajIme(string poruka)
@@ -84,6 +83,8 @@ namespace Ucenje
                     Console.WriteLine("Unos smije sadržavati samo slova.");
                 }
             }
+
+
         }
 
     }
